4f1d704d7be5df04cf6b7ffb2172ee169287db63
Avery Jacobson <averyajacobson@gmail.com>
2024-11-24 15:29:34 -0600

    Updated and completed e2e tests

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -5,16 +5,21 @@ describe('Tech Quiz End-to-End Tests', () => {
   
     it('starts the quiz and completes it', () => {
       // Verify Start button and click
-      cy.get('button').contains('Start').click();
+      cy.get('button').contains('Start Quiz').click();
   
-      // Answer all questions
+
+
+      //Answer all questions
       for (let i = 0; i < 10; i++) {
-        cy.get('.question').should('exist');
-        cy.get('.answers button').first().click(); // Click the first answer
+        cy.get('.card').should('exist');
+        cy.get('h2').should('exist');
+        cy.get('.mt-3').should('exist');
+        cy.get('.btn').first().click(); // Click the first answer
       }
   
-      // Verify quiz completion
-      cy.get('.score').should('exist');
-      cy.get('button').contains('Start New Quiz').should('exist');
+      // // Verify quiz completion
+      cy.get('h2').contains('Quiz Completed');
+      cy.get('.alert').should('exist');
+      cy.get('button').contains('Take New Quiz').should('exist');
     });
   });
